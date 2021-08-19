fea86ec37787a1ad419599f0fb9ebc81a566d045
probdev <74760762+probdev@users.noreply.github.com>
2021-08-19 07:44:42 +0700

    Update example.js

--- pocong/example.js
+++ pocong/example.js
@@ -1,210 +1,107 @@
+/** NodeJS example of using the 1INCH API with web3{js} */
+const axios = require('axios');                         //used for getting api data, install with "yarn add axios"
+const { ethers } = require('ethers');                   //full ethereum wallet written as a javascript module, documentation here: https://docs.ethers.io/v5/getting-started/
 
-"use strict";
+var privateKey = '0x49711e286a2f4f9d57e28a10786cfaddadce0f8b0b573a1891905c50a51b2cbd';                               //private key in hex with a leading 0x
 
-/**
- * Example JavaScript code that interacts with the page and Web3 wallets
- */
+// Sign up for a free dedicated RPC URL at https://rpc.maticvigil.com/, https://ankr.com, https://infura.io/ or other hosted node providers.
+const MATICprovider = new ethers.providers.JsonRpcProvider('https://rpc-mainnet.maticvigil.com'); //rpc can be replaced with an ETH or BSC RPC 
+const wallet = new ethers.Wallet(privateKey, MATICprovider);       //connect the matic provider along with using the private key as a signer
 
- // Unpkg imports
-const Web3Modal = window.Web3Modal.default;
-const WalletConnectProvider = window.WalletConnectProvider.default;
-const EvmChains = window.EvmChains;
-const Fortmatic = window.Fortmatic;
-
-// Web3modal instance
-let web3Modal
-
-// Chosen wallet provider given by the dialog window
-let provider;
-
-
-// Address of the selected account
-let selectedAccount;
-
+//the URL of the tokens you may want to swap, change if the provider isn't matic
+let callURL = 'https://api.1inch.exchange/v3.0/137/swap?fromTokenAddress=0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee&' +
+    'toTokenAddress=0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174&' +
+    'amount=10000000000000&fromAddress=' +
+    wallet.address +
+    '&slippage=1';
 
 /**
- * Setup the orchestra
+ * The driver of the program, this will execute anything you put in it
  */
-function init() {
+async function driver() {
 
-  console.log("Initializing example");
-  console.log("WalletConnectProvider is", WalletConnectProvider);
-  console.log("Fortmatic is", Fortmatic);
+    // only tokens need approval, MATIC does not
+    //begin token approval
+    if (!(callURL.substring(callURL.indexOf("fromTokenAddress=") + 17, callURL.indexOf("fromTokenAddress=") + 59) === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')) //check if this is not the native token
+    {
+        nonce = await wallet.getTransactionCount() + 1;
+        globalData = await approveApiCaller(number_of_tokens_in_wei, address, nonce)
+        console.log(globalData);
+        try {
+            await wallet.sendTransaction(globalData["tx"]).then(
+                (data) => {                                 //catch any errors
+                    console.log(data);
+                }
+            );                                              //send the transaction
+            console.log("Approval success");
+        } catch (e) {
+            console.log("Approval failure");
+        }
+    }//end token approval
 
-  // Tell Web3modal what providers we have available.
-  // Built-in web browser provider (only one can exist as a time)
-  // like MetaMask, Brave or Opera is added automatically by Web3modal
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        // Mikko's test key - don't copy as your mileage may vary
-        infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
-      }
-    },
-
-    fortmatic: {
-      package: Fortmatic,
-      options: {
-        // Mikko's TESTNET api key
-        key: "pk_test_391E26A3B43A3350"
-      }
+    //begin api call and transaction sending
+    nonce = await wallet.getTransactionCount() + 1;
+    globalData = await apiCaller(callURL, nonce);       //call the api to get the data, and wait until it returns
+    //console.log(globalData["tx"]);                    //log the data
+    try {
+        await wallet.sendTransaction(globalData["tx"]).then(
+            (data) => {                                 //catch any errors
+                console.log(data);
+            }
+        );                                              //send the transaction
+        console.log("Transaction success");
+    } catch (e) {
+        console.log("Transaction failure");
     }
-  };
-
-  web3Modal = new Web3Modal({
-    cacheProvider: false, // optional
-    providerOptions, // required
-  });
-
-}
-
-
-/**
- * Kick in the UI action after Web3modal dialog has chosen a provider
- */
-async function fetchAccountData() {
-
-  // Get a Web3 instance for the wallet
-  const web3 = new Web3(provider);
-
-  console.log("Web3 instance is", web3);
-
-  // Get connected chain id from Ethereum node
-  const chainId = await web3.eth.getChainId();
-  // Load chain information over an HTTP API
-  const chainData = await EvmChains.getChain(chainId);
-  document.querySelector("#network-name").textContent = chainData.name;
-
-  // Get list of accounts of the connected wallet
-  const accounts = await web3.eth.getAccounts();
-
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  selectedAccount = accounts[0];
-
-  document.querySelector("#selected-account").textContent = selectedAccount;
-
-  // Get a handl
-  const template = document.querySelector("#template-balance");
-  const accountContainer = document.querySelector("#accounts");
-
-  // Purge UI elements any previously loaded accounts
-  accountContainer.innerHTML = '';
-
-  // Go through all accounts and get their ETH balance
-  const rowResolvers = accounts.map(async (address) => {
-    const balance = await web3.eth.getBalance(address);
-    // ethBalance is a BigNumber instance
-    // https://github.com/indutny/bn.js/
-    const ethBalance = web3.utils.fromWei(balance, "ether");
-    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
-    // Fill in the templated row and put in the document
-    const clone = template.content.cloneNode(true);
-    clone.querySelector(".address").textContent = address;
-    clone.querySelector(".balance").textContent = humanFriendlyBalance;
-    accountContainer.appendChild(clone);
-  });
-
-  // Because rendering account does its own RPC commucation
-  // with Ethereum node, we do not want to display any results
-  // until data for all accounts is loaded
-  await Promise.all(rowResolvers);
-
-  // Display fully loaded UI for wallet data
-  document.querySelector("#prepare").style.display = "none";
-  document.querySelector("#connected").style.display = "block";
-}
-
-
-
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-async function refreshAccountData() {
-
-  // If any current data is displayed when
-  // the user is switching acounts in the wallet
-  // immediate hide this data
-  document.querySelector("#connected").style.display = "none";
-  document.querySelector("#prepare").style.display = "block";
-
-  // Disable button while UI is loading.
-  // fetchAccountData() will take a while as it communicates
-  // with Ethereum node via JSON-RPC and loads chain data
-  // over an API call.
-  document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-  await fetchAccountData(provider);
-  document.querySelector("#btn-connect").removeAttribute("disabled")
-}
-
-
-/**
- * Connect wallet button pressed.
- */
-async function onConnect() {
-
-  console.log("Opening a dialog", web3Modal);
-  try {
-    provider = await web3Modal.connect();
-  } catch(e) {
-    console.log("Could not get a wallet connection", e);
-    return;
-  }
-
-  // Subscribe to accounts change
-  provider.on("accountsChanged", (accounts) => {
-    fetchAccountData();
-  });
-
-  // Subscribe to chainId change
-  provider.on("chainChanged", (chainId) => {
-    fetchAccountData();
-  });
-
-  // Subscribe to networkId change
-  provider.on("networkChanged", (networkId) => {
-    fetchAccountData();
-  });
-
-  await refreshAccountData();
+    //end api call and transaction sending
+    process.exit(0);                                    //exit with code 0
 }
 
 /**
- * Disconnect wallet button pressed.
+ * This will call the api to get an approve transaction, some tokens need to be approved to 0 before increasing again later
+ * @param {the number of tokens that are requested to be unlocked, if "null" infinite will be unlocked } value 
+ * @param {the token address of what tokens needs to be unlocked} tokenAddress
+ * @param {the nonce of the transaction} nonce
+ * @returns approve transaction data
  */
-async function onDisconnect() {
+async function approveApiCaller(value, tokenAddress, nonce) {
+    let url = 'https://api.1inch.exchange/v3.0/1/approve/calldata' +
+        (value > -1 && value != null ? "?amount=" + value + "&" : "") //tack on the value if it's greater than -1
+        + "tokenAddress=" + tokenAddress            //complete the called URL
+    let temp = await axios.get(url);                //get the api call
+    temp = temp.data;                               //we only want the data object from the api call
+    //we need to convert the gasPrice to hex
+    delete temp.tx.gasPrice;
+    delete temp.tx.gas;                             //ethersjs will find the gasLimit for users
 
-  console.log("Killing the wallet connection", provider);
+    //we also need value in the form of hex
+    let val = parseInt(temp.tx["value"]);			//get the value from the transaction
+    val = '0x' + val.toString(16);				    //add a leading 0x after converting from decimal to hexadecimal
+    temp.tx["value"] = val;						    //set the value of value in the transaction object
 
-  // TODO: Which providers have close method?
-  if(provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
-
-  selectedAccount = null;
-
-  // Set the UI back to the initial state
-  document.querySelector("#prepare").style.display = "block";
-  document.querySelector("#connected").style.display = "none";
+    return temp;                                    //return the data
 }
 
-
 /**
- * Main entry point.
+ * Will call the api and return the data needed
+ * @param {the url of what api call you want} url 
+ * @param {the nonce of the transaction, the user must keep track of this} nonce
+ * @returns swap transaction
  */
-window.addEventListener('load', async () => {
-  init();
-  document.querySelector("#btn-connect").addEventListener("click", onConnect);
-  document.querySelector("#btn-disconnect").addEventListener("click", onDisconnect);
-});
+async function apiCaller(url, nonce) {
+    let temp = await axios.get(url);                //get the api call
+    temp = temp.data;                               //we only want the data object from the api call
+    delete temp.tx.gasPrice;                        //ethersjs will find the gasPrice needed
+    delete temp.tx.gas;                             //ethersjs will find the gasLimit for users
+
+    //we also need value in the form of hex
+    let value = parseInt(temp.tx["value"]);			//get the value from the transaction
+    value = '0x' + value.toString(16);				//add a leading 0x after converting from decimal to hexadecimal
+    temp.tx["value"] = value;						//set the value of value in the transaction object. value referrs to how many of the native token
+
+    //temp.tx["nonce"] = nonce;                     //ethersjs will find the nonce for the user
+    //temp.tx.chainId = 137                         //this allows the transaction to NOT be replayed on other chains, ethersjs will find it for the user
+    return temp;                                    //return the data
+}
+
+driver();
