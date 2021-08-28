4fdddeb3a06fac341e5d733579ac45f6a3e21c95
probdev <74760762+probdev@users.noreply.github.com>
2021-08-29 05:57:19 +0700

    Update deriv.js

--- pocong/deriv.js
+++ pocong/deriv.js
@@ -215,7 +215,7 @@ ws.onmessage = function(msg) {
 			console.log('Lot : ' + lot + '');
 			var random_angka = Math.floor(Math.random() * 10) + 2;
 				//ws.send(JSON.stringify({authorize:'GUD9xZiCshPrIpL'}));
-			ws.send(JSON.stringify({"buy":1,"parameters":{"amount":"" + lot + "","app_markup_percentage":"2","barrier":1,"basis":"stake","contract_type":"DIGITOVER","currency":"USD","duration":random_angka,"duration_unit":"t","symbol":"1HZ25V"},"price":"" + lot + ""}));
+			ws.send(JSON.stringify({"buy":1,"parameters":{"amount":"" + lot + "","app_markup_percentage":"2","barrier":1,"basis":"stake","contract_type":"DIGITOVER","currency":"USD","duration":random_angka,"duration_unit":"t","symbol":"1HZ25V"}}));
             //ws.send(JSON.stringify({"buy":1,"parameters":{"amount":"" + lot + "","app_markup_percentage":"2","barrier":8,"basis":"stake","contract_type":"DIGITUNDER","currency":"USD","duration":1,"duration_unit":"t","symbol":"1HZ25V"},"price":"" + lot + ""}));
 				//ws.send(JSON.stringify({authorize:'gqtfnLSQqaNWKKc'}));
 			//document.querySelector("#myBtn1").click();
