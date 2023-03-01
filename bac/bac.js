b367a995e29c426f6a1140e1fcf336a9df309a53
probdev <74760762+probdev@users.noreply.github.com>
2023-03-01 08:22:31 +0700

    Add files via upload

---
+++ bac/bac.js
@@ -0,0 +1,35 @@
+
+document.getElementById("start").addEventListener("click", function MyFunction3(evt){
+
+var p_satu = document.querySelector("#p1").value;
+var p_dua = document.querySelector("#p2").value;
+var p_tiga = document.querySelector("#p3").value;
+
+var b_satu = document.querySelector("#b1").value;
+var b_dua = document.querySelector("#b1").value;
+var b_tiga = document.querySelector("#b1").value;
+
+
+if (p_dua != ""){
+var hitung_p_satu = parseInt(b_dua) + (parseInt(p_dua) + parseInt(p_tiga));
+var hitung_p_dua = parseInt(b_dua);
+var hitung_p_result = hitung_p_satu + hitung_p_dua;
+console.log('Result : ', hitung_p_result);
+var hitung_b_satu = parseInt(p_dua) + (parseInt(b_dua) + parseInt(b_tiga));
+var hitung_b_dua = parseInt(p_dua);
+var hitung_b_result = hitung_b_satu + hitung_b_dua;
+
+if (hitung_p_result > hitung_b_result){
+console.log('Result : PLAYER');
+document.querySelector("#result").innerText = "PLAYER";
+} else {
+console.log('Result : BANKER');
+document.querySelector("#result").innerText = "BANKER";
+
+}
+
+
+}
+
+
+});
